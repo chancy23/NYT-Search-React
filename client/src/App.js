c6efebe211982174d8eb223330f13b0cc7b5a16d
Chancy <chancyleath@hotmail.com>
2018-12-31 13:58:19 -0700

    add Main page class and moved state and items from App to it, made conditional for if results and saved articles

--- client/src/App.js
+++ client/src/App.js
@@ -1,110 +1,15 @@
-import React, { Component } from 'react';
+import React from 'react';
+import Main from './Pages/Main';
 import Navbar from './components/Navbar';
-import Results from './components/Results';
-import Search from './components/Search';
-import Saved from './components/Saved';
 import './App.css';
-import API from './utils/API';
 
-class App extends Component {
-  state = {
-    query: '',
-    startYear: '',
-    endYear: '',
-    results: [],
-    savedArticles: []
-  };
-
-  componentDidMount() {
-    //if saved articles call the method to load them to the page
-    this.getSavedArticles();
-  };
-
-  //include the this.state.query, startYear and endYear as params
-  getArticles = () => {
-    //format years to match the format required by NYT
-    const formattedStartYear = this.state.startYear + '0101';
-    const formattedEndYear = this.state.endYear + '1231';
-    API.getArticles(this.state.query, formattedStartYear, formattedEndYear)
-    .then((response) =>{
-      console.log("start date", formattedStartYear);
-      console.log("end date", formattedEndYear);
-      this.setState({results: response.data.response.docs});
-      console.log('results', this.state.results);
-    })
-    .catch(err => console.log(err))
-  };
-
-  handleInput = (event) => {
-    const {name, value} = event.target;
-    this.setState({
-      [name]: value
-    });
-  };
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    this.getArticles();
-  };
-
-  //need to fix this route, look at gitlab activities
-  handleSaveArticle = (id) => {
-    //this gets the props._id for the article that the button was clicked on and grabs the data associated with it
-    const findArticleByID = this.state.results.find((el) => el._id === id);
-    const articleToSave = {title: findArticleByID.headline.main, url: findArticleByID.web_url, date: findArticleByID.pub_date};
-    console.log('article to save data', articleToSave);
-    //call api method to save article (need to send the articles headline, link, and pub date)
-    API.saveArticle(articleToSave)
-      .then(this.getSavedArticles())
-      .catch(err => console.log(err))
-  };
-
-  getSavedArticles = () => {
-    API.getSavedArticles()
-      .then(response => {
-        // console.log("res from db", res);
-        this.setState({savedArticles: response.data})
-      })
-      .catch(err => console.log(err))
-  };
-
-  handleDelete = (id) => {
-    //get the article ID for the article delete button is on
-    const findArticleByID = this.state.savedArticles.find((el) => el._id === id);
-    const articleID = findArticleByID._id;
-    // console.log('article id', articleID);
-    API.deleteArticle(id)
-      .then(() => {
-        console.log('article removed from db:', articleID);
-        this.getSavedArticles()
-      })
-      .catch(err => console.log(err))
-  };
-
-  render() {
-    return (
-      <div>
-        <Navbar />
-        <div className='container'>
-          <Search 
-            query={this.state.query}
-            startYear={this.state.startYear}
-            endYear={this.state.endYear}
-            handleInput={this.handleInput}
-            handleSubmit={this.handleSubmit}
-          />
-          <Results 
-            data={this.state.results}
-            handleSaveArticle={this.handleSaveArticle} 
-          />
-          <Saved 
-            data={this.state.savedArticles}
-            handleDelete={this.handleDelete}
-          />
-        </div>
-      </div>
-    );
-  }
-}
+const App = () => {
+  return (
+    <div>
+      <Navbar />
+      <Main />
+    </div>
+  );
+};
 
 export default App;
---
+++ client/src/Pages/Main/Main.js
@@ -0,0 +1,113 @@
+import React, { Component } from 'react';
+import Results from '../../components/Results';
+import Search from '../../components/Search';
+import Saved from '../../components/Saved';
+// import './App.css';
+import API from '../../utils/API';
+
+class Main extends Component {
+  state = {
+    query: '',
+    startYear: '',
+    endYear: '',
+    results: [],
+    savedArticles: []
+  };
+
+  componentDidMount() {
+    //if saved articles call the method to load them to the page
+    this.getSavedArticles();
+  };
+
+  //include the this.state.query, startYear and endYear as params
+  getArticles = () => {
+    //format years to match the format required by NYT
+    const formattedStartYear = this.state.startYear + '0101';
+    const formattedEndYear = this.state.endYear + '1231';
+    API.getArticles(this.state.query, formattedStartYear, formattedEndYear)
+      .then((response) => {
+        console.log("start date", formattedStartYear);
+        console.log("end date", formattedEndYear);
+        this.setState({ results: response.data.response.docs });
+        console.log('results', this.state.results);
+      })
+      .catch(err => console.log(err))
+  };
+
+  handleInput = (event) => {
+    const { name, value } = event.target;
+    this.setState({
+      [name]: value
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    this.getArticles();
+  };
+
+  //need to fix this route, look at gitlab activities
+  handleSaveArticle = (id) => {
+    //this gets the props._id for the article that the button was clicked on and grabs the data associated with it
+    const findArticleByID = this.state.results.find((el) => el._id === id);
+    const articleToSave = { title: findArticleByID.headline.main, url: findArticleByID.web_url, date: findArticleByID.pub_date };
+    console.log('article to save data', articleToSave);
+    //call api method to save article (need to send the articles headline, link, and pub date)
+    API.saveArticle(articleToSave)
+      .then(this.getSavedArticles())
+      .catch(err => console.log(err))
+  };
+
+  getSavedArticles = () => {
+    API.getSavedArticles()
+      .then(response => {
+        // console.log("res from db", res);
+        this.setState({ savedArticles: response.data })
+      })
+      .catch(err => console.log(err))
+  };
+
+  handleDelete = (id) => {
+    //get the article ID for the article delete button is on
+    const findArticleByID = this.state.savedArticles.find((el) => el._id === id);
+    const articleID = findArticleByID._id;
+    // console.log('article id', articleID);
+    API.deleteArticle(id)
+      .then(() => {
+        console.log('article removed from db:', articleID);
+        this.getSavedArticles()
+      })
+      .catch(err => console.log(err))
+  };
+
+  render() {
+    return (
+      <div className='container'>
+        <Search
+          query={this.state.query}
+          startYear={this.state.startYear}
+          endYear={this.state.endYear}
+          handleInput={this.handleInput}
+          handleSubmit={this.handleSubmit}
+        />
+        {this.state.results.length ? (
+          <Results
+            data={this.state.results}
+            handleSaveArticle={this.handleSaveArticle}
+          /> ) : (
+            <h5>No Results to Display, use Search Above</h5>
+          )}
+        {this.state.savedArticles.length ? (
+          <Saved
+            data={this.state.savedArticles}
+            handleDelete={this.handleDelete}
+          /> ) : (
+            <h5>No Saved Articles</h5>
+          )}
+        
+      </div>
+    );
+  }
+}
+
+export default Main;
